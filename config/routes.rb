7fed7f1cbdf6f68ff6718ba39757545d81b0c619
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2020-05-15 04:39:23 +0000

    Fix rubocop errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'users#index'
 
-  resources :users, only: %I(:index, :new, :create, :edit, :update)
+  resources :users, only: %I[:index :new :create :edit :update]
 end
