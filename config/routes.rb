5f399040e9b1791533b67a457fd5443b7e5b532a
FrancoRosa <km115.franco@gmail.com>
2020-05-14 21:07:57 -0500

    Add index view and method

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
+  root to: 'users#index'
+  resources :users, only: [:index, :new, :create, :edit, :update]
+  #resources :users
 end
